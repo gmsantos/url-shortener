e1e79220d167ccb60e8f3acee267c4a22a8297c2
gmsantos <gabriel.ms1@hotmail.com>
2016-11-15 17:09:47 -0200

    Create tests for User endpoint

--- tests/Integration/UserTest.php
+++ tests/Integration/UserTest.php
@@ -3,14 +3,50 @@
 namespace App\Test\Integration;
 
 use App\Test\TestCase;
+use App\User;
 use Laravel\Lumen\Testing\DatabaseTransactions;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Illuminate\Http\Response;
 
 class UserTest extends TestCase
 {
-    use DatabaseTransactions;
+    use DatabaseMigrations, DatabaseTransactions;
 
-    public function testShouldBeOk()
+    public function testShouldCreateAnUser()
     {
-        $this->assertTrue(true);
+        $this->json('POST', '/users', ['id' => 'Sally'])
+            ->seeJson(['id' => 'Sally'])
+            ->seeStatusCode(Response::HTTP_CREATED);
+
+        $this->seeInDatabase('users', ['id' => 'Sally']);
+    }
+
+    public function testShouldReturnConflictWhenCreateAnExistingUser()
+    {
+        factory(User::class)->create(['id' => 'Sally']);
+
+        $this->seeInDatabase('users', ['id' => 'Sally']);
+
+        $this->json('POST', '/users', ['id' => 'Sally'])
+            ->seeJson(['id' => 'Sally'])
+            ->seeStatusCode(Response::HTTP_CONFLICT);
+    }
+
+    public function testShouldSuccessfulRemoveAnUser()
+    {
+        factory(User::class)->create(['id' => 'Sally']);
+
+        $this->seeInDatabase('users', ['id' => 'Sally']);
+
+        $this->json('DELETE', '/users/Sally')
+            ->seeStatusCode(Response::HTTP_OK);
+
+        $this->notSeeInDatabase('users', ['id' => 'Sally']);
+    }
+
+    public function testShouldNotRemoveAnAbsentUser()
+    {
+        $this->json('DELETE', '/users/Sally')
+            ->seeStatusCode(Response::HTTP_NOT_FOUND);
     }
 }
